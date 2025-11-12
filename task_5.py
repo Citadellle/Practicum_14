c47f6bcdc263cf8bea18735b422466945584bcaf
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-12 12:14:33 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,5 +1,23 @@
-user_list = [int(i) for i in input().split(' ')]
+def calculate_average(user_list: list) -> float:
+    '''
+    Calculate the average value of a list of numbers.
+    
+    Args:
+        numbers (list): List of numbers to calculate average for
+    
+    Returns:
+        float: The average value of the numbers
+    '''
+    average_val = sum(user_list) / len(user_list)
+  
+    return average_val
 
-average_val = sum(user_list) / len(user_list)
 
-print(average_val)
+def main():
+    user_list = [int(i) for i in input().split(' ')]
+    average = calculate_average(user_list)
+    print(average)
+
+
+if __name__ == '__main__':
+    main()
