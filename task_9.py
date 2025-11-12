5c7851e2f817514ac8d7f7a5aaf26ada10c835b1
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-12 12:04:50 +0700

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,13 +1,15 @@
+import string
+
 words_dict = {}
 
-word = input().lower().strip('.,:;?!-()')
+word = input().lower().strip(string.punctuation)
 while word != '':
     if word not in words_dict:
         words_dict[word] = 1
     else:
         words_dict[word] += 1
 
-    word = input().lower().strip('.,:;?!-()')
+    word = input().lower().strip(string.punctuation)
 
 words_dict_sorted = sorted(words_dict.items(),
                            key=lambda i: i[1],
