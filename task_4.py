30f3a809fd489c750efea4c992007dffc7c4618f
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-12 12:11:38 +0700

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,9 +1,28 @@
 import re
 import string
 
-pattern = re.escape(string.punctuation)
-user_list = re.split(f'[{pattern} ]+', input())
+def get_unique_words(text: str) -> list:
+    '''
+    Split text into words, remove punctuation and return unique words.
+    
+    Args:
+        str: Input text to process
+    
+    Returns:
+        list: List of unique words without punctuation
+    '''
+    pattern = re.escape(string.punctuation)
+    user_list = re.split(f'[{pattern} ]+', text)
+    new_list = list(set(user_list))
+  
+    return new_list
 
-new_list = list(set(user_list))
 
-print(new_list)
+def main():
+    text = input()
+    unique_words = get_unique_words(text)
+    print(unique_words)
+
+
+if __name__ == '__main__':
+    main()
