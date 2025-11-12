02d67df72c4aa54c93aa58a851ff2c91901d3ca9
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-12 11:54:33 +0700

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,15 +1,30 @@
-num_3_in_list = False
+def remove_number_3() -> list:
+    '''
+    Continuously prompts user for input until a list with exactly one '3' is provided.
+    Then removes the number 3 from the list and returns the modified list.
+    Returns:
+        list: The original user list with the number 3 removed
+    '''
+    num_3_in_list = False
 
-while num_3_in_list == False:
-    user_list = [int(i) for i in input().split(' ')]
+    while num_3_in_list == False:
+        user_list = [int(i) for i in input().split(' ')]
 
-    if user_list.count(3) != 1:
-        print('Неверный ввод, введи строку,' \
-              'которая содержит только одно число 3')
-        continue
+        if user_list.count(3) != 1:
+            print('Неверный ввод, введи строку, которая содержит только одно число 3')
+            continue
 
-    num_3_in_list = True
+        num_3_in_list = True
 
-user_list.remove(3)
+    user_list.remove(3)
 
-print(user_list)
+    return user_list
+
+
+def main():
+    result_list = remove_number_3()
+    print(result_list)
+
+
+if __name__ == '__main__':
+    main()
