897e35840fbc4371d1512c85fa493331731901e3
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-12 12:17:56 +0700

    Update task_6.py

--- task_6.py
+++ task_6.py
@@ -1,10 +1,28 @@
-num = int(input())
+def search_divisors(number: int) -> list:
+    '''
+    Find all divisors of a given number.
+    
+    Args:
+        int: The number for which divisors are being searched
+    
+    Returns:
+        list: List of all divisors of the number
+    '''
+    divisors = set()
+    
+    for i in range(1, int(number ** 0.5) + 1):
+        if number % i == 0:
+            divisors.add(i)
+            divisors.add(number // i)
+    
+    return sorted(list(divisors))
 
-divs = set()
 
-for i in range(1, int(num ** 0.5) + 1):
-    if num % i == 0:
-        divs.add(i)
-        divs.add(num // i)
+def main():
+    num = int(input())
+    divisors = search_divisors(num)
+    print(divisors)
 
-print(sorted(list(divs)))
+
+if __name__ == '__main__':
+    main()
