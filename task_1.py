38fd0d1d8393894a8c4bc8b71a6cef17dd072d9e
Mikhail Gordeev <miha_gord@mail.ru>
2025-11-12 11:49:00 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,7 +1,29 @@
-user_list = [int(input()) for i in range(10)]
+def calculate_sums(user_list: list) -> nist:
+    '''
+    Calculate sums of adjacent elements in a list.
+    
+    Args:
+        list: list of numbers to process
+    Returns:
+        list: new list where each element is the sum of two adjacent elements
+              from the user_list
+    Example:
+        Input: [1, 2, 3, 4]
+        Output: [3, 5, 7]
+    '''
+    new_list = []
+    for i in range(len(user_list) - 1):
+        new_list.append(user_list[i] + user_list[i+1])
+    return new_list
 
-new_list = []
-for i in range(len(user_list) - 1):
-    new_list.append(user_list[i] + user_list[i+1])
 
-print(new_list)
+def main():
+    user_list = [int(input()) for i in range(10)]
+    
+    result_list = calculate_sums(user_list)
+
+    print(result_list)
+
+
+if __name__ == '__main__':
+    main()
